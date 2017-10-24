6b6b514ce321e739536448b5f2f14ac03ff0ceac
ChuckConde <chuck.conde@gmail.com>
2017-10-24 12:14:15 -0300

    practice 2

--- 01-JS-DOM-APIs/new.js
+++ 01-JS-DOM-APIs/new.js
@@ -2,3 +2,7 @@ function showNinja(){
     document.getElementById("ninja").style.visibility = "visible";
 }
 window.onload = showNinja;
+
+function callSamurai(){
+    alert("I'm a samurai, nothing like ninja");
+}
