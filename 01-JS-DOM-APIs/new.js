6480b2038362ab93ee5d353cbec17ea66b171743
ChuckConde <chuck.conde@gmail.com>
2017-10-25 11:31:46 -0300

    exercise 3

--- 01-JS-DOM-APIs/new.js
+++ 01-JS-DOM-APIs/new.js
@@ -6,3 +6,35 @@ window.onload = showNinja;
 function callSamurai(){
     alert("I'm a samurai, nothing like ninja");
 }
+function getConfig() {
+    config = {
+        method: "GET",
+        url: "http://api.icndb.com/jokes/random",
+        async: "true"
+    };
+    return onClick(config);
+}
+
+function onClick(config) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open(config.method, config.url, config.async);
+    xhttp.onload = function (e) {
+        if (this.readyState == 4 && this.status == 200) {
+            const response = JSON.parse(xhttp.response);
+            const resp = response.value;
+            console.log(resp.id);
+            console.log(resp.joke);
+            document.getElementById("ninja").innerText = resp.joke;
+        } else {
+            console.error(xhttp.statusText);
+            document.getElementById("ninja").innerText = xhttp.statusText;
+            document.getElementById("ninja").style.backgroundColor = "red";
+        }
+    }
+    xhttp.onerror = function (e) {
+        console.error(xhttp.statusText);
+        document.getElementById("ninja").innerText = xhttp.statusText;
+        document.getElementById("ninja").style.backgroundColor = "red";
+    };
+    xhttp.send();
+};
