4ef893cbc1cf44c9c93e6f524ede5c8fd27cdcec
ChuckConde <chuck.conde@gmail.com>
2017-11-14 12:57:38 -0300

    fixes

--- Topic 02/src/eventEmitter.js
+++ Topic 02/src/eventEmitter.js
@@ -3,11 +3,8 @@ export class EventEmitter {
         this.events = {};
     }
     on(eventName, callback) {
-        if (this.events[eventName] = this.events[eventName] || []) {
-            this.events[eventName].push(callback);
-        } else {
-            this.events[eventName] = [callback];
-        }
+        this.events[eventName] = this.events[eventName] || [];
+        this.events[eventName].push(callback)
     };
     emit(eventName) {
         if (this.events[eventName]) {
--- Topic 02/src/movie.js
+++ Topic 02/src/movie.js
@@ -20,18 +20,15 @@ class Movie extends EventEmitter {
     addCast(cast) {
         if (cast instanceof Actor) {
             this._cast.push(cast);
-        }
-        else if (Array.map(cast)) {
+        } else if (Array.map(cast)) {
             for (let i = 0; i < cast.length; i++) {
                 if (cast[i] instanceof Actor) {
                     this._cast.push(cast[i]);
-                }
-                else {
+                } else {
                     console.log(`What are you trying to do? Of course is an Error, ${cast[i]} isn't an Actor`);
                 }
             }
-        }
-        else {
+        } else {
             console.log(`Nope, ${cast} certanly isn't an actor`);
         }
     }
